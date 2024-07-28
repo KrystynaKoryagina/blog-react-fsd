fe38492063ab9fe0e241378cf04997dc9f20373f
Krystyna Koryagina <krystyna.koryagina@gmail.com>
2024-07-28 22:55:39 +0300

    Removed condole.log and fix console errors

--- src/features/AddCommentForm/model/selectors/getAddCommentFormText/getAddCommentFormText.ts
+++ src/features/AddCommentForm/model/selectors/getAddCommentFormText/getAddCommentFormText.ts
@@ -1,4 +1,4 @@
 import { StoreSchema } from '@/app/providers/store';
 
 export const getAddCommentFormText = (state: StoreSchema) =>
-  state.addCommentForm?.comment;
+  state.addCommentForm?.comment || '';
--- src/features/Search/ui/Search/Search.tsx
+++ src/features/Search/ui/Search/Search.tsx
@@ -1,6 +1,5 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
-// import { useDebounce } from 'shared/lib/hooks/useDebounce';
 import { Card } from '@/shared/ui/Card';
 import { Input } from '@/shared/ui/Input';
 
@@ -8,20 +7,11 @@ interface SearchProps {
   searchValue?: string;
   onSearch: (value: string) => void;
   className?: string;
-  // TODO
-  // onDeleteSearch
 }
 
 export const Search = memo(
   ({ searchValue = '', className, onSearch }: SearchProps) => {
-    // TODO
     const { t } = useTranslation();
-
-    console.log('Search');
-
-    // console.log('searchValue', searchValue);
-
-    // const onChangeHandler = useDebounce(onSearch, 700);
 
     return (
       <Card className={className}>
